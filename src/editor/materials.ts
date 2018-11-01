7ecdc32c3a6dd5e650d4eb8f5c9d1e4fe83e98ef
igoriev36 <igoryev26@gmail.com>
2018-11-01 12:53:44 +0000

    WIP

---
+++ src/editor/materials.ts
@@ -0,0 +1,6 @@
+import { MeshBasicMaterial } from "three";
+
+export const meshMaterial = new MeshBasicMaterial({
+  wireframe: true,
+  color: "pink"
+});
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,14 @@
+import { getRoot, onSnapshot, types } from "mobx-state-tree";
 import { fromEvent, merge } from "rxjs";
 import { debounceTime, share, startWith, throttleTime } from "rxjs/operators";
 import * as THREE from "three";
 import { addControls } from "./editor/controls";
 
-const pressedKeys = new Set();
-
 const GRID_SIZE = 3;
-const NUMBER_OF_GRIDS = 41;
+const NUMBER_OF_GRIDS = 71;
 
+const pressedKeys = new Set();
 const objects = [];
-
-const meshMaterial = new THREE.MeshBasicMaterial({
-  wireframe: true,
-  color: "pink"
-});
 
 function make({
   gridX = 300,
@@ -56,6 +51,16 @@ const sgeo = new THREE.ExtrudeGeometry(shape, extrudeSettings);
 // setup 3D scene
 
 const scene = new THREE.Scene();
+scene.add(
+  new THREE.ArrowHelper(
+    new THREE.Vector3(0, 1, 0),
+    new THREE.Vector3(),
+    40,
+    0xff0000,
+    5,
+    3
+  )
+);
 const container = document.getElementById("scene");
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -63,7 +68,7 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
-camera.position.set(-30, 100, -70);
+camera.position.set(-30, 100, 70);
 camera.lookAt(0, 1, 0);
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
@@ -91,8 +96,8 @@ var grid = new THREE.GridHelper(
   new THREE.Color(0xdddddd),
   new THREE.Color(0xdddddd)
 );
-grid.position.x = -1.5;
-grid.position.z = -1.5;
+grid.position.x = -GRID_SIZE / 2;
+grid.position.z = -GRID_SIZE / 2;
 scene.add(grid);
 
 var geometry = new THREE.PlaneBufferGeometry(
@@ -144,6 +149,7 @@ var rollOverMesh = new THREE.Mesh(
 scene.add(rollOverMesh);
 
 let pos = new THREE.Vector3();
+let xyz = [0, 0, 0];
 
 container.appendChild(renderer.domElement);
 
@@ -153,49 +159,67 @@ function handleMouseMove(event) {
   vector.set(mouse2D.x, mouse2D.y, camera.near);
 
   raycaster.setFromCamera(mouse2D, camera);
-  intersects = raycaster.intersectObjects(objects);
 
-  if (intersects.length > 0) {
-    pos
-      .copy(intersects[0].point)
-      .add(intersects[0].face.normal)
-      .divideScalar(12)
-      .floor()
-      .multiplyScalar(12)
-      .addScalar(6);
+  if (pressedKeys.has("Shift")) {
+    intersects = raycaster.intersectObjects(objects);
 
-    rollOverMesh.position.copy(pos);
+    if (intersects.length > 0) {
+      const { x, z } = pos
+        .copy(intersects[0].point)
+        .divideScalar(GRID_SIZE)
+        .floor();
+      xyz = [x, 0, z + 1];
+      console.log(xyz);
+
+      pos
+        .copy(intersects[0].point)
+        .add(intersects[0].face.normal)
+        .divideScalar(12)
+        .floor()
+        .multiplyScalar(12)
+        .addScalar(6);
+
+      rollOverMesh.position.copy(pos);
+    }
+  } else if (pressedKeys.has("Alt")) {
+    intersects = raycaster.intersectObjects(objects.slice(1));
+    if (intersects.length > 0) {
+      console.log(intersects[0]);
+    }
+  } else {
+    intersects = [];
   }
 }
 
 function handleClick(event) {
   if (pressedKeys.has("Shift") && intersects.length > 0) {
-    const cube = new THREE.Mesh(sgeo, cubeMat);
-    cube.receiveShadow = true;
-    cube.castShadow = true;
-    const s = new THREE.Box3().setFromObject(cube);
-    const container = new THREE.Object3D();
-    const box = new THREE.Mesh(
-      new THREE.BoxBufferGeometry(
-        s.max.x - s.min.x,
-        s.max.y - s.min.y,
-        s.max.z - s.min.z
-      ),
-      meshMaterial
-    );
-    container.add(cube);
-    container.add(box);
-    cube.position.x -= (s.max.x - s.min.x) / 2;
-    cube.position.z -= (s.max.z - s.min.z) / 2;
-    cube.position.y -= (s.max.y - s.min.y) / 2;
-    container.position.copy(pos);
-    // s.position.copy(pos);
-    scene.add(container);
-    // scene.add(cube);
-    objects.push(box);
-    setTimeout(() => {
-      handleMouseMove(event);
-    }, 50);
+    // const cube = new THREE.Mesh(sgeo, cubeMat);
+    // cube.receiveShadow = true;
+    // cube.castShadow = true;
+    // const s = new THREE.Box3().setFromObject(cube);
+    // const container = new THREE.Object3D();
+    // const box = new THREE.Mesh(
+    //   new THREE.BoxBufferGeometry(
+    //     s.max.x - s.min.x,
+    //     s.max.y - s.min.y,
+    //     s.max.z - s.min.z
+    //   ),
+    //   meshMaterial
+    // );
+    // container.add(cube);
+    // container.add(box);
+    // cube.position.x -= (s.max.x - s.min.x) / 2;
+    // cube.position.z -= (s.max.z - s.min.z) / 2;
+    // cube.position.y -= (s.max.y - s.min.y) / 2;
+    // container.position.copy(pos);
+    // // s.position.copy(pos);
+    // scene.add(container);
+    // // scene.add(cube);
+    // objects.push(box);
+    // setTimeout(() => {
+    //   handleMouseMove(event);
+    // }, 50);
+    window.s.addSubAssembly(...xyz);
   }
 }
 
@@ -215,6 +239,13 @@ keyDown$.subscribe(handleKeyDown);
 const keyUp$ = fromEvent(window, "keyup").pipe(share());
 keyUp$.subscribe(handleKeyUp);
 
+merge(keyUp$, keyDown$)
+  .pipe(startWith(null))
+  .subscribe(() => {
+    console.log(pressedKeys);
+    rollOverMesh.visible = pressedKeys.has("Shift");
+  });
+
 const wheel$ = fromEvent(renderer.domElement, "wheel");
 
 const click$ = fromEvent(renderer.domElement, "click").pipe(share());
@@ -226,10 +257,6 @@ const mouseMove$ = fromEvent(renderer.domElement, "mousemove").pipe(
 );
 mouseMove$.subscribe(handleMouseMove);
 
-merge(keyUp$, keyDown$)
-  .pipe(startWith(null))
-  .subscribe(() => (rollOverMesh.visible = pressedKeys.has("Shift")));
-
 merge(mouseMove$, wheel$, keyUp$, keyDown$, click$, resize$)
   .pipe(
     startWith(null),
@@ -238,3 +265,131 @@ merge(mouseMove$, wheel$, keyUp$, keyDown$, click$, resize$)
   .subscribe(render);
 
 const controls = addControls(camera, renderer.domElement);
+
+// mobx-state-tree ------------------------------------------
+
+const SubAssembly = types
+  .model("SubAssembly", {
+    id: types.identifier,
+    x: types.number,
+    y: types.number,
+    z: types.number
+  })
+  .views(self => ({
+    get isTouchingGround() {
+      return self.y === 0;
+    },
+    isOverlapping(other) {
+      return self.bbox.intersectsBox(other.bbox);
+    },
+    get scene() {
+      return getRoot(self);
+    }
+  }))
+  .volatile(self => ({
+    get box() {
+      const [width, height, depth] = [
+        GRID_SIZE * 16,
+        GRID_SIZE * 4,
+        GRID_SIZE * 4
+      ];
+      const geom = new THREE.BoxBufferGeometry(width, height, depth);
+      geom.translate(width / 2, height / 2, -depth / 2);
+      const mesh = new THREE.Mesh(
+        geom,
+        new THREE.MeshBasicMaterial({ color: "pink" })
+      );
+      mesh.position.set(
+        self.x * GRID_SIZE,
+        self.y * GRID_SIZE,
+        self.z * GRID_SIZE
+      );
+      return mesh;
+    },
+    get bbox() {
+      return new THREE.Box3().setFromObject(self.box);
+    }
+  }))
+  .actions(self => ({
+    afterAttach() {
+      scene.add(self.box);
+    },
+    beforeDestroy() {
+      scene.remove(self.box);
+    }
+  }));
+
+const Scene = types
+  .model("Scene", {
+    gridSize: types.optional(types.number, GRID_SIZE),
+    subAssemblies: types.array(SubAssembly)
+  })
+  .volatile(self => ({
+    get scene() {
+      return scene;
+    }
+  }))
+  .views(self => ({
+    canPlace(subAssembly) {
+      return true;
+      // if (self.subAssemblies.some(s => s.isOverlapping(subAssembly))) {
+      //   return false;
+      // } else {
+      //   if (subAssembly.isTouchingGround) {
+      //     return true;
+      //     // if (isOverlapping) {
+      //     //   return false
+      //     // } else {
+      //     //   return true
+      //     // }
+      //   } else {
+      //     return false;
+      //     // const isHovering =
+
+      //     // if (!isHovering) {
+      //     //   return true
+      //     // } else {
+      //     //   return false
+      //     // }
+      //   }
+      // }
+    }
+  }))
+  .actions(self => ({
+    afterCreate() {
+      console.log("created scene");
+      onSnapshot(self, snapshot => {
+        localStorage.setItem("scene", JSON.stringify(snapshot));
+      });
+    },
+    addSubAssembly(x, y, z) {
+      const sub = SubAssembly.create({
+        id: Math.random().toString(),
+        x,
+        y,
+        z
+      });
+
+      if (self.canPlace(sub)) {
+        self.subAssemblies.push(sub);
+      }
+    },
+    removeSubAssemblies() {
+      while (self.subAssemblies.length > 0) {
+        self.subAssemblies.remove(self.subAssemblies[0]);
+      }
+    }
+  }));
+
+const s = Scene.create(JSON.parse(localStorage.getItem("scene") || "{}"));
+window["s"] = s;
+s.addSubAssembly(-8, 0, 2);
+s.addSubAssembly(-8, 4, 2);
+// s.addSubAssembly(5, 0, 10);
+// s.addSubAssembly(3, 0, 0);
+// s.addSubAssembly(10, 0, 0);
+// s.addSubAssembly(20, 0, 0);
+
+// setTimeout(() => {
+//   s.removeSubAssemblies();
+// }, 400);
